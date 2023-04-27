cbac26529926d9b65da5718c8c94c72f080ddea6
Victor Yuri <victoryuritc@yahoo.com>
2023-04-27 00:52:40 -0300

    fix: import on `HomeView`

--- src/atomic/pages/HomeView/HomeView.tsx
+++ src/atomic/pages/HomeView/HomeView.tsx
@@ -2,7 +2,7 @@ import AdvantagesSection from "./sections/AdvantagesSection/AdvantagesSection";
 import DestinationsSection from "./sections/DestinationsSection/DestinationsSection";
 import DownloadAndContactSection from "./sections/DownloadAndContactSection/DownloadAndContactSection";
 import MoreExperiencesSection from "./sections/MoreExperiencesSection/MoreExperiencesSection";
-import { PageContainer, SectionWrapper } from "./styles";
+import { PageContainer } from "./styles";
 
 export default function HomeView() {
   return (
